a7cbc3561b5664a6b0d130d9f23d47d987a5442d
Igor Bari <mucsi96@gmail.com>
2024-05-05 10:53:33 +0000

    simplify avatar example

--- src/Avatar/Avatar.stories.ts
+++ src/Avatar/Avatar.stories.ts
@@ -7,20 +7,18 @@ const meta: Meta = {
     html`<header bt>
         <nav>
           <a href="/"><h1 bt>App</h1></a>
-          <section>
-            <button bt-avatar id="avatar" popovertarget="avatar-popover">
-              JD
-            </button>
-            <div popover bt id="avatar-popover">
-              <ul bt-dropdown-menu>
-                <li bt-separated>
-                  <p>John Doe</p>
-                  <p>john.doe@authelia.com</p>
-                </li>
-                <li><button type="button">Sign out</button></li>
-              </ul>
-            </div>
-          </section>
+          <button bt-avatar id="avatar" popovertarget="avatar-popover">
+            JD
+          </button>
+          <div popover bt id="avatar-popover">
+            <ul bt-dropdown-menu>
+              <li bt-separated>
+                <p>John Doe</p>
+                <p>john.doe@authelia.com</p>
+              </li>
+              <li><button type="button">Sign out</button></li>
+            </ul>
+          </div>
         </nav>
       </header>
       <div style="min-height: 50px"></div>`,
