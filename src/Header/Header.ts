b41a590fcb6630b4e5b19926663f7e795b195f7a
Igor Bari <mucsi96@gmail.com>
2024-05-06 18:55:53 +0000

    make header overflowing

--- src/Header/Header.ts
+++ src/Header/Header.ts
@@ -1,4 +1,30 @@
-import { injectStyles } from '../utils';
+import { injectStyles, onElementConnected } from '../utils';
 import styles from './Header.css?raw&inline';
 
 injectStyles(styles);
+
+onElementConnected('header[bt] > nav > ul', (menu) => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        menu.classList.remove('overflowing');
+      } else {
+        menu.classList.add('overflowing');
+      }
+    },
+    {
+      root: null,
+      rootMargin: '0px 0px 0px 0px',
+      threshold: [0.99],
+    },
+  );
+
+  const lastLi = menu.querySelector('li:last-child');
+
+  if (!lastLi) {
+    return;
+  }
+
+  observer.observe(lastLi);
+});
